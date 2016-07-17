2d35f71bfb71d101cf9425db4955333755bcf99a
Bharatt Kukreja <bkukreja@uwaterloo.ca>
2016-07-16 20:10:13 -0400

    Added the uml's

--- textui.h
+++ textui.h
@@ -1,11 +1,16 @@
 #ifndef __TEXTUI_H__
 #define __TEXTUI_H__
 #include "ui.h"
+#include <iostream>
 
 class TextUI : public UI {
+    istream &in;
+    ostream &out;
     public:
+        TextUI();
         CommandType getNextInput() override;
-        void output(vector <vector <Cell>>) override;
+        void output(vector <vector <Cell>> grid) override;
+
 }
 
 #endif
--- ui.h
+++ ui.h
@@ -1,18 +1,15 @@
 #ifndef __UI_H__
 #define __UI_H__
 #include "ui.h"
-<<<<<<< HEAD
 #include "commands.h"
 #include "cell.h"
-=======
->>>>>>> 5803d250cb7534d7b0a2c8950775f7c31bba403b
-#include "vector.h"
+#include <vector.h>
 
 
 class UI {
     public:
         CommandType getNextInput();
-        void output(std::vector <std::vector <Cell>>);
+        void output(std::vector <std::vector <Cell>> grid);
 }
 
 #endif
