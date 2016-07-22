832822222ef089cd606a518d3d28dc5708913029
Aidan Day <Aidan Day>
2016-07-22 18:39:51 -0400

    adding back atkpot.{h,cc}

---
+++ atkpot.h
@@ -0,0 +1,21 @@
+#ifndef __ATKPOT_H_
+#define __ATKPOT_H_
+
+#include "potion.h"
+
+class PC;
+
+const int valueAtkPot = 5;
+bool visiblePosAtkPot = false;
+bool visibleNegAtkPot = false;
+
+class AtkPot : public Potion {
+ public:
+    void use(PC & user) override; // AtkPots change their user's attack
+    explicit AtkPot(const bool positive); // value is a default value and isVisible comes with them
+    AtkPot(const AtkPot & other);
+    AtkPot & operator=(const AtkPot & other);
+    ~AtkPot() {};
+};
+
+#endif
