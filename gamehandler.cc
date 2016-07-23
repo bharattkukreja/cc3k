8c3842d0d3257b1e3be5b9a79e7b5d59df6691b8
Bharatt Kukreja <bkukreja@uwaterloo.ca>
2016-07-23 13:03:52 -0400

    fixed gamehandler

--- gamehandler.cc
+++ gamehandler.cc
@@ -3,9 +3,14 @@
 #include "gamemap.h"
 #include "pc.h"
 #include "human.h"
-#include "elf.h"
+/*#include "elf.h"
 #include "dwarf.h"
 #include "orc.h"
+*/
+
+#include <fstream>
+#include <sstream>
+#include <utility>
 
 using namespace std;
 
@@ -16,14 +21,14 @@ void GameHandler::play(string file) {
     ui.output_message("Enter the character you want to play with");
     CommandType c = ui.getNextInput();
     if(c == CommandType::h)
-        hero = new Human();
-    else if(c == CommandType::e)
+        hero = shared_ptr <PC> (new Human());
+    /*else if(c == CommandType::e)
         hero = new Elf();
     else if(c == CommandType::d)
         hero = new Dwarf();
     else
         hero = new Orc();
-    
+    */
     vector <vector <CellType>> layout;
     
     ifstream fin(file);
@@ -64,29 +69,29 @@ void GameHandler::play(string file) {
     g.initialize(hero);
     
     while(true) {
-        pair <CommandType CommandType> command_pair;
+        pair <CommandType, CommandType> command_pair;
         CommandType input1 = CommandType::no_value;
         CommandType input2 = ui.getNextInput();
         if(input2 == CommandType::a || input2 == CommandType::d) {
             input1 = ui.getNextInput();
         }       
         else if(input2 == CommandType::r) {
-            delete hero;
+            // delete hero;
             ui.output_message("Restarting the game");
-            play();
+            play(file);
         }
         else if(input2 == CommandType::q) {
-            delete hero;
+            // delete hero;
             ui.output_message("Thanks for playing the game");
             break;
         }
         else if(g.getFloorCount() > 5) {
-            delete hero;
+            // delete hero;
             ui.output_message("You Won! Congratulations!");
             break;
         }
-        else if(hero->hp == 0) {
-            delete hero;
+        else if(hero->getHP() == 0) {
+            // delete hero;
             ui.output_message("You Lost!");
             break;
         }
