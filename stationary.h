074d24920b1f365f52594ce596adaf6b072a4ba2
aidanday <aidanday4@gmail.com>
2016-07-23 19:12:22 -0400

    public enemy #1: private destructors

--- stationary.h
+++ stationary.h
@@ -4,6 +4,7 @@
 #include "sprite.h"
 
 class Stationary : public Sprite {
+ public:
     virtual ~Stationary() = 0;
 }; // this class only exists to contain both treasure
 // and stairs, which are stationary and pointed to by tiles
