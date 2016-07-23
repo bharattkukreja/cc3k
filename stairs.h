4f72dc1e46d39a605fe316bc7cebfd4c766a30ce
Aidan Day <Aidan Day>
2016-07-23 14:02:44 -0400

    added stairs.h

---
+++ stairs.h
@@ -0,0 +1,9 @@
+#ifndef __STAIRS_H_
+#define __STAIRS_H_
+
+#include "stationary.h"
+
+class Stairs : public Stationary {}; // yay empty classes~~
+// this exists so GameMap knows when to make end a floor
+
+#endif
