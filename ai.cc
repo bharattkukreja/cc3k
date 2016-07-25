43fe5152f02bf954d02fbed9d1ad8c5690b2a185
bkukreja <bkukreja@student.cs.uwaterloo.ca>
2016-07-24 23:48:13 -0400

    fixed ai class

--- ai.cc
+++ ai.cc
@@ -1,10 +1,17 @@
 #include "ai.h"
 #include <cstdlib>
+#include "sprite.h"
+#include "spritetype.h"
 
 using namespace std;
 
 void AI::move(vector <pair <int, int>> &enemyLocations, vector <vector <Cell>> &grid) {
     for (unsigned int i = 0; i < enemyLocations.size(); i++) {
+        
+        Cell c = grid[enemyLocations[i].first][enemyLocations[i].second];
+        if(c.sprite->getType() == SpriteType::Dragon)
+            continue;
+        
         int x = enemyLocations[i].first;
         int y = enemyLocations[i].second;
         
@@ -19,7 +26,7 @@ void AI::move(vector <pair <int, int>> &enemyLocations, vector <vector <Cell>> &
             unsigned int y_coord = y + rand2;
 
             if((x_coord >= 0) && (y_coord >= 0) && (x_coord <= grid.size()) && (y_coord <= grid.at(0).size())) {
-                if(grid[x_coord][y_coord].getType() == CellType::Floor) {
+                if(grid[x_coord][y_coord].getType() == CellType::Floor && grid[x_coord][y_coord].isEmpty()) {
                     grid[x_coord][y_coord].sprite = grid[x][y].sprite;
                     grid[x][y].sprite = nullptr;
 		    enemyLocations[i].first = x_coord;
@@ -31,5 +38,4 @@ void AI::move(vector <pair <int, int>> &enemyLocations, vector <vector <Cell>> &
     }
 }
 
-//AI::AI(vector <vector <Cell>> &grid) : grid {grid} {}
 
