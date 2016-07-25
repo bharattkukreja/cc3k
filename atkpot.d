866fdc2f5b190751969ac5f367a72e55df9603a6
bkukreja <bkukreja@student.cs.uwaterloo.ca>
2016-07-25 15:29:13 -0400

    potion to 3 types now

--- atkpot.d
+++
@@ -1,2 +0,0 @@
-atkpot.o: atkpot.cc atkpot.h potion.h item.h sprite.h spritetype.h \
- stationary.h pc.h character.h
--- item.d
+++
@@ -1,2 +0,0 @@
-item.o: item.cc item.h sprite.h spritetype.h stationary.h pc.h \
- character.h
