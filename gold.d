6f91b4bb845e1db267f5d70005cd3cc6959ae78b
bkukreja <bkukreja@student.cs.uwaterloo.ca>
2016-07-25 22:06:14 -0400

    everything works now

--- gold.d
+++
@@ -1,2 +0,0 @@
-gold.o: gold.cc gold.h item.h sprite.h spritetype.h stationary.h pc.h \
- character.h
--- hppot.d
+++
@@ -1,2 +0,0 @@
-hppot.o: hppot.cc spritetype.h hppot.h potion.h item.h sprite.h \
- stationary.h pc.h character.h
